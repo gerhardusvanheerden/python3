1a0a79b1650a6838cda8c0838454ba1e724edd81
gerhardusvanheerden <gerhardusvanheerden@gmail.com>
2017-03-09 11:01:33 +0000

    test the push
    try again

--- pythoncourse/exercise_3_1.py
+++ pythoncourse/exercise_3_1.py
@@ -1,4 +1,10 @@
-lst=["Terribly Tricky"]
+""" 
+	----------------------------------
+	2017-02-25
+	test
+	----------------------------------
+"""
+lst=["this is just a test"]
 for word in lst:
     for letter in word[-6:]:
         print(letter)
